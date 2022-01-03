ad492513b83e3bbab02be390d67869f6f4c7bac9
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-01-03 12:12:13 -0600

    Initial Commit TG2G

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var main = function() {
+    
+    
+    
+    
+}
+
+$(document).ready(main);
+
+
+
+confirm('Thank you for considering Texas Go 2 Guys for your future move. We aim to provide a hassle-free moving experience!!!');
